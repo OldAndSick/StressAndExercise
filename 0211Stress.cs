a30963ac47cb7c107efa75004b4ad67bf104ade0
OldAndSick <sps990311naver.com@gmail.com>
2026-02-11 17:41:43 +0900

    Create 0211Stress.cs

---
+++ 0211Stress.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Solve3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // 입력한 값의 나누기 4를하고 나온 수만큼 long을 출력
+            int push = int.Parse(Console.ReadLine());
+            int a = push / 4;
+            
+            for(int i = 0; i<a; i++)
+            {
+                Console.Write("long ");
+            }
+            Console.Write("int");
+
+
+        }
+    }
+}
